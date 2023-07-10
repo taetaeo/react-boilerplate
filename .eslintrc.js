fb4303e7be42e4d1ee5ceb2cf86b9c6599189f5f
ohtaekwon <ohtk1119@gmail.com>
2023-07-10 18:39:44 +0900

    스토리북 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,29 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true,
+    jest: true
   },
   ignorePatterns: ["*.js"],
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "prettier",
-  ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:react-hooks/recommended", "plugin:import/recommended", "plugin:jsx-a11y/recommended", "plugin:@typescript-eslint/recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking", "airbnb-typescript", "airbnb/hooks", "prettier", "plugin:storybook/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 12,
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: "./tsconfig.json"
   },
   plugins: ["@typescript-eslint"],
   rules: {
@@ -36,28 +25,28 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      { vars: "all", args: "after-used", ignoreRestSiblings: false },
-    ],
-    "@typescript-eslint/explicit-function-return-type": "warn", // Consider using explicit annotations for object literals and function return types even when they can be inferred.
-    "@typescript-eslint/naming-convention": [
-      "error",
-      // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables (23.10)
-      {
-        selector: "variable",
-        format: ["camelCase", "PascalCase", "UPPER_CASE"],
-      },
-      // Allow camelCase functions (23.2), and PascalCase functions (23.8)
-      {
-        selector: "function",
-        format: ["camelCase", "PascalCase"],
-      },
-      // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make TypeScript recommendations, we are assuming this rule would similarly apply to anything "type like", including interfaces, type aliases, and enums
-      {
-        selector: "typeLike",
-        format: ["PascalCase"],
-      },
-    ],
-  },
+    "@typescript-eslint/no-unused-vars": ["error", {
+      vars: "all",
+      args: "after-used",
+      ignoreRestSiblings: false
+    }],
+    "@typescript-eslint/explicit-function-return-type": "warn",
+    // Consider using explicit annotations for object literals and function return types even when they can be inferred.
+    "@typescript-eslint/naming-convention": ["error",
+    // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables (23.10)
+    {
+      selector: "variable",
+      format: ["camelCase", "PascalCase", "UPPER_CASE"]
+    },
+    // Allow camelCase functions (23.2), and PascalCase functions (23.8)
+    {
+      selector: "function",
+      format: ["camelCase", "PascalCase"]
+    },
+    // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make TypeScript recommendations, we are assuming this rule would similarly apply to anything "type like", including interfaces, type aliases, and enums
+    {
+      selector: "typeLike",
+      format: ["PascalCase"]
+    }]
+  }
 };
